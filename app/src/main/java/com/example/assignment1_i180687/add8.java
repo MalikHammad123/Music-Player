cceb5c556aee257f9fa69d6908aec4397ed02acd
MalikHammad123 <103526110+MalikHammad123@users.noreply.github.com>
2022-09-18 23:12:45 +0500

    hi

---
+++ app/src/main/java/com/example/assignment1_i180687/add8.java
@@ -0,0 +1,14 @@
+package com.example.assignment1_i180687;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add8);
+    }
+}
