f3b134e5daeeef2ec5fba6a0b6cc20126be0e6c1
MalikHammad123 <103526110+MalikHammad123@users.noreply.github.com>
2022-09-19 00:55:56 +0500

    hi

--- app/src/main/java/com/example/assignment1_i180687/MainActivity.java
+++ app/src/main/java/com/example/assignment1_i180687/MainActivity.java
@@ -2,13 +2,195 @@ package com.example.assignment1_i180687;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b4=findViewById(R.id.button4);
+        b5=findViewById(R.id.button5);
+        b6=findViewById(R.id.button6);
+        b7=findViewById(R.id.button7);
+        b8=findViewById(R.id.button8);
+        b9=findViewById(R.id.button9);
+        b10=findViewById(R.id.button10);
+        b11=findViewById(R.id.button11);
+        b12=findViewById(R.id.button12);
+        b13=findViewById(R.id.button13);
+        b14=findViewById(R.id.button14);
+        b15=findViewById(R.id.button15);
+        b16=findViewById(R.id.button16);
+        b17=findViewById(R.id.button17);
+        b18=findViewById(R.id.button18);
+        b19=findViewById(R.id.button19);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,splash1.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,signup2.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,signin3.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,fb4.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,playlist5.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,songtitle6.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,navdrawer7.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,add8.class);
+                startActivity(intent);
+
+                onBackPressed();
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,upload9.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Record10.class);
+                startActivity(intent);
+                onBackPressed();
+
+            }
+        });
+        b11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,songtitle11.class);
+                startActivity(intent);
+                onBackPressed();
+
+            }
+        });
+        b12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,despacito12a.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,despacito13.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,recentsearch14.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,listenlater15.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,listenlater16.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,liked17.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,profile18.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        b19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,last.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+
+
+
     }
 }
